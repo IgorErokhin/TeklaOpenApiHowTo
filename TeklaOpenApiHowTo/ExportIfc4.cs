3243ae4ce070e144941ef9404ad0d560ae46bd8f
Erokhin Igor <igor.erokhin@trimble.com>
2021-09-24 11:33:48 +0300

    ExportIfc4

---
+++ TeklaOpenApiHowTo/ExportIfc4.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Tekla.Structures.ModelInternal;
+
+namespace TeklaOpenApiHowTo
+{
+    public class ExportIfc4_
+    {
+        public static bool ExportIfc4(bool isExportSelected, string modelName, string fullFileName, List<string> propertySets, string exportLayerAs, string objectColoring)
+        {
+            Operation.IFCExportFlags flags = new Operation.IFCExportFlags
+            {
+                IsFlatBeamsAsPlates = false,
+                IsLocationFromOrganizer = false,
+                IsPoursEnabled = false
+            };
+
+            if (isExportSelected)
+            {
+                return Operation.ExportIFCFromSelected(modelName, fullFileName, Operation.IFCExportViewTypeEnum.UNDEFINED, propertySets, Operation.ExportBasePoint.BASE_POINT,
+                    exportLayerAs, objectColoring, flags);
+            }
+            else
+            {
+                return Operation.ExportIFCFromAll(modelName, fullFileName, Operation.IFCExportViewTypeEnum.UNDEFINED, propertySets, Operation.ExportBasePoint.BASE_POINT,
+                    exportLayerAs, objectColoring, flags);
+            }
+        }
+    }
+}
--- TeklaOpenApiHowTo/FindOutSolidsIntersect.cs
+++ TeklaOpenApiHowTo/FindOutSolidsIntersect.cs
@@ -28,7 +28,6 @@ namespace TeklaOpenApiHowTo
                 };
             };
 
-
             return false;
         }
     }
