930f598d5cf1dcf3206e572cc9d5b3a8d1bf59cf
Erokhin Igor <igor.erokhin@trimble.com>
2021-10-08 15:47:37 +0300

    Transform Point to another TransformationPlane

---
+++ TeklaOpenApiHowTo/TransformPointToAnotherTransformationPlane.cs
@@ -0,0 +1,13 @@
+﻿using Tekla.Structures.Geometry3d;
+using Tekla.Structures.Model;
+
+namespace TeklaOpenApiHowTo
+{
+    public class TransformPointToAnotherTransformationPlane_
+    {
+        public static Point TransformPointToAnotherTransformationPlane(Point point, TransformationPlane startPlane, TransformationPlane goalPlane)
+        {
+            return new Point(goalPlane.TransformationMatrixToLocal.Transform(startPlane.TransformationMatrixToGlobal.Transform(point)));
+        }
+    }
+}
