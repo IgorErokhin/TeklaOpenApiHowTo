3ed250c1edc8f1181d3ae84268d7ac971f2e7385
Erokhin Igor <igor.erokhin@trimble.com>
2021-10-08 15:41:04 +0300

    Insert left or right aligned text

---
+++ TeklaOpenApiHowTo/InsertAlignedText.cs
@@ -0,0 +1,32 @@
+﻿using Tekla.Structures.Drawing;
+using Tekla.Structures.Geometry3d;
+
+namespace TeklaOpenApiHowTo
+{
+    public class InsertAlignedText
+    {
+        public static Text InsertLeftAlignedText(ViewBase view, Point insertionPoint, string text, Text.TextAttributes textAttributes)
+        {
+            Text textObj = new Text(view, insertionPoint, text, new PointPlacing(), textAttributes);
+            textObj.Insert();
+
+            RectangleBoundingBox bbox = textObj.GetObjectAlignedBoundingBox();
+            textObj.MoveObjectRelative(new Vector(bbox.Width / 2, 0, 0));
+            textObj.Modify();
+
+            return textObj;
+        }
+
+        public static Text InsertRightAlignedText(ViewBase view, Point insertionPoint, string text, Text.TextAttributes textAttributes)
+        {
+            Text textObj = new Text(view, insertionPoint, text, new PointPlacing(), textAttributes);
+            textObj.Insert();
+
+            RectangleBoundingBox bbox = textObj.GetObjectAlignedBoundingBox();
+            textObj.MoveObjectRelative(new Vector(-(bbox.Width / 2), 0, 0));
+            textObj.Modify();
+
+            return textObj;
+        }
+    }
+}
