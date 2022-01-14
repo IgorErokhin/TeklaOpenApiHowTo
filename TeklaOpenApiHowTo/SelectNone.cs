700bb0be87f76970687990c9469e98895528e116
Erokhin Igor <igor.erokhin@trimble.com>
2022-01-14 10:53:46 +0300

    Add SelectNone class

---
+++ TeklaOpenApiHowTo/SelectNone.cs
@@ -0,0 +1,13 @@
+﻿using Tekla.Structures.Model.UI;
+
+namespace TeklaOpenApiHowTo
+{
+    public class SelectNone_
+    {
+        public static void SelectNone()
+        {
+            ModelObjectSelector selector = new ModelObjectSelector();
+            selector.Select(new System.Collections.ArrayList { });
+        }
+    }
+}
